6bcf04e3d2e3e6b2342e997772d15c76ca3e8ae1
glynac-dataeng <dataeng@glynac.ai>
2026-05-20 17:21:01 +0530

    Update create_table.sql

--- intern-project/Aadya Anil Kumar/project-DHAP-34/Conversations Dataset for ChatBot/config/create_table.sql
+++ intern-project/Aadya Anil Kumar/project-DHAP-34/Conversations Dataset for ChatBot/config/create_table.sql
@@ -1,1 +1,6 @@
-
+CREATE TABLE IF NOT EXISTS public.chatbot_conversations (
+    id        INTEGER NOT NULL,
+    question  TEXT    NOT NULL,
+    answer    TEXT    NOT NULL,
+    PRIMARY KEY (id)
+);
